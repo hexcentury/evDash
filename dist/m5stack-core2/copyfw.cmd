d6a533b4380b4ffc0fa863b3d3ed4539920170f0
Ivan Y. Smirnov <ivan.y.smirnov@yadro.com>
2024-04-21 13:48:21 +0300

    copy fw script

---
+++ dist/m5stack-core2/copyfw.cmd
@@ -0,0 +1,1 @@
+copy /Y C:\Users\ivan.y.smirnov\Documents\Private\m5stack\evDash\.pio\build\m5stack-core2\firmware.bin C:\Users\ivan.y.smirnov\Documents\Private\m5stack\evDash\dist\m5stack-core2\evDash.ino.bin 
